795c28a2fa2f5ea1bbc2e226e2857a578f05834d
Void-sleepy <hosainallam999@gmail.com>
2025-01-02 15:02:18 +0200

    Update database.py

--- SOFTproj/PROJfiles/database.py
+++ SOFTproj/PROJfiles/database.py
@@ -4,10 +4,10 @@ from supabase import create_client, Client
 class SupabaseClient:
     SUPABASE_URL = "https://jaqghporpakiqmktsrir.supabase.co"
     ANON_KEY = (
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4c2NzcmhvaGd5Zm94dHdkaXl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0MTM2MzEsImV4cCI6MjA0Mzk4OTYzMX0.632pdeTGRvqn_qAl3gWV4zonQ1inUzaiFVpu1IUnMQ0"
+        ""
     )
     SERVICE_ROLE_KEY = (
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4c2NzcmhvaGd5Zm94dHdkaXl4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODQxMzYzMSwiZXhwIjoyMDQzOTg5NjMxfQ.4CORmDY_dzOtzSv1ulugMajE7dU9yrDMGpZrg4Mu4zw"
+        ""
     )
 
     def __init__(self):
